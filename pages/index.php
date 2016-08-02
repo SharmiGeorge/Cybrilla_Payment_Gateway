a3303d13eafec17e4472e3be23ff6e00de689ccc
anusha-giftxoxo <anusha@giftxoxo.com>
2016-08-02 17:45:33 +0530

    On branch demodev 	modified:   pages/index.php

--- pages/index.php
+++ pages/index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Anusha Yammanur</title>
+    <title>Anusha</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
